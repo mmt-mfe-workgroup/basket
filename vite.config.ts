f141d7fa38d90ba651af6dfca1f8d2df8ec97b68
Harvey Gomez <hmgomez90@gmail.com>
2024-01-23 15:54:08 +0000

    Add vite-plugin-federation

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: 'mfe-basket-app',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App.tsx',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
+});
